870729be0fc5905a04ff841f4cd0ff7d7c4558f2
leesh96 <suho2718@gmail.com>
2021-10-06 13:29:41 +0900

    #32 [feature] : LoginActivity와 실제 서버 연동
    구글 로그인의 id와 이름으로 서버에 로그인 요청을 보낸 후 응답을 받는 코드 작성
    LoginViewModel 작성
    하드 코딩 String을 String value로 변경

--- app/src/main/java/com/pns/bbaspassenger/view/LoginActivity.kt
+++ app/src/main/java/com/pns/bbaspassenger/view/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.pns.bbaspassenger.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
@@ -8,8 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.pns.bbaspassenger.viewmodel.LoginViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.pns.bbaspassenger.R
 import com.pns.bbaspassenger.databinding.ActivityLoginBinding
+import com.pns.bbaspassenger.viewmodel.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -22,7 +25,11 @@ class LoginActivity : AppCompatActivity() {
                 val account = task.getResult(ApiException::class.java)
                 val uid = account.id
                 val name = account.displayName
-                Log.d(TAG, "로그인 성공 : $uid, $name")
+                if (uid != null) {
+                    viewModel.sign(uid, name?: "")
+                } else {
+                    loginFailed()
+                }
             } catch (e: ApiException) {
                 Log.e(TAG, e.toString())
                 e.printStackTrace()
@@ -39,8 +46,22 @@ class LoginActivity : AppCompatActivity() {
             viewModel.onClickLogin(googleSignIn())
         }
 
+        setObserver()
+    }
+
+    private fun setObserver() {
         viewModel.googleSignInEvent.observe(this) {
             it.getContentIfNotHandled()
+        }
+
+        viewModel.loginSuccess.observe(this) {
+            if (it) {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                loginFailed()
+            }
         }
     }
 
@@ -51,6 +72,16 @@ class LoginActivity : AppCompatActivity() {
         val googleSignInIntent = mGoogleSignInClient.signInIntent
 
         startForSignInResult.launch(googleSignInIntent)
+    }
+
+    private fun loginFailed() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.login_failed_title))
+            .setMessage(getString(R.string.login_failed_message))
+            .setPositiveButton(getString(R.string.btn_confirm)) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            .show()
     }
 
     companion object {
--- app/src/main/java/com/pns/bbaspassenger/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/pns/bbaspassenger/viewmodel/LoginViewModel.kt
@@ -1,16 +1,56 @@
 package com.pns.bbaspassenger.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pns.bbaspassenger.data.model.SignUpRequestBody
+import com.pns.bbaspassenger.repository.UserRepository
+import com.pns.bbaspassenger.utils.BBasGlobalApplication
 import com.pns.bbaspassenger.utils.SingleEvent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class LoginViewModel : ViewModel() {
     private val _googleSignInEvent = MutableLiveData<SingleEvent<Unit>>()
+    private val _loginSuccess = MutableLiveData<Boolean>()
 
     val googleSignInEvent: LiveData<SingleEvent<Unit>> = _googleSignInEvent
+    val loginSuccess: LiveData<Boolean> = _loginSuccess
 
     fun onClickLogin(action: Unit) {
         _googleSignInEvent.value = SingleEvent(action)
+    }
+
+    fun sign(userId: String, name: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val signUpRequestBody = SignUpRequestBody(userId, name)
+            try {
+                UserRepository.sign(signUpRequestBody).let { response ->
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            val user = it.result
+                            Log.d(TAG, "result : $user")
+
+                            BBasGlobalApplication.prefs.setUserPrefs(user)
+                            Log.d(TAG, "prefs user : ${BBasGlobalApplication.prefs.getUserPrefs()}")
+                            _loginSuccess.postValue(true)
+                        }
+                    } else {
+                        Log.d(TAG, "result : ${response.code()}")
+                        _loginSuccess.postValue(false)
+                    }
+                }
+            } catch (e : IOException) {
+                Log.e(TAG, e.message.toString())
+                e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "Login"
     }
 }
