6090b0cb13a03af7562bdc7d45d996e4dd1f67c9
jaeseuk <sjs4126@naver.com>
2021-10-06 21:46:50 +0900

    #23 [ui/ux] : MainActivity xml 작성
    MainActivity 클래스
    -> 액션바 설정과 액션바 메뉴 inflate, 클릭 이벤트 추가
    activity_main.xml 작성
    
    **추가
    strings.xml에 리소스 추가
    액션바 메뉴 menu_actionbar.xml 추가
    검색, 새로고침, 마이페이지, 비상연락, 정류장 아이콘 추가
    검색창 배경 추가

--- app/src/main/java/com/pns/bbaspassenger/view/MainActivity.kt
+++ app/src/main/java/com/pns/bbaspassenger/view/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.pns.bbaspassenger.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.pns.bbaspassenger.R
+import com.pns.bbaspassenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.tbMain)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_actionbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.btn_my_page -> {
+                //TODO 마이페이지 이동
+                true
+            }
+            R.id.btn_emergency -> {
+                //TODO 비상 연락
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
