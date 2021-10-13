81cdce274140110ebd368e6e45a83fd92c919f0b
leesh96 <suho2718@gmail.com>
2021-10-13 21:51:14 +0900

    #71 [feature] : 앱 실행 시 예약정보 받아오는 부분 추가
    예약 정보 존재하면 탑승 액티비티로, 취소하는 경우 예약정보 삭제 후 메인 액티비티 이동
    존재하지 않으면 메인 액티비티로

--- app/src/main/java/com/pns/bbaspassenger/view/LoginActivity.kt
+++ app/src/main/java/com/pns/bbaspassenger/view/LoginActivity.kt
@@ -56,11 +56,49 @@ class LoginActivity : AppCompatActivity() {
 
         viewModel.loginSuccess.observe(this) {
             if (it) {
-                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+                viewModel.getQueue()
             } else {
                 loginFailed()
+            }
+        }
+
+        viewModel.isQueueExist.observe(this) {
+            it.getContentIfNotHandled()?.let { res ->
+                if (res) {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("탑승 예약 정보 존재")
+                        .setMessage("진행 중인 탑승 정보가 존재합니다.\n계속 하시겠습니까?\n[확인]을 누르면 이어할 수 있습니다.\n[취소]를 누르면 삭제됩니다.")
+                        .setPositiveButton(getString(R.string.btn_confirm)) { dI, _ ->
+                            dI.dismiss()
+                            startBoardActivity()
+                        }
+                        .setNegativeButton(getString(R.string.btn_cancel)) { dI, _ ->
+                            viewModel.deleteQueue()
+                            dI.dismiss()
+                        }
+                        .setCancelable(false)
+                        .show()
+                } else {
+                    startMainActivity()
+                }
+            }
+        }
+
+        viewModel.queueDelete.observe(this) {
+            it.getContentIfNotHandled()?.let { res ->
+                if (res) {
+                    startMainActivity()
+                } else {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("오류 발생")
+                        .setMessage("예상치 못한 오류가 발생하여 앱이 종료됩니다.")
+                        .setPositiveButton(getString(R.string.btn_confirm)) { dI, _ ->
+                            dI.dismiss()
+                            finish()
+                        }
+                        .setCancelable(false)
+                        .show()
+                }
             }
         }
     }
@@ -83,6 +121,18 @@ class LoginActivity : AppCompatActivity() {
             }
             .setCancelable(false)
             .show()
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun startBoardActivity() {
+        val intent = Intent(this, OnBoardActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     companion object {
