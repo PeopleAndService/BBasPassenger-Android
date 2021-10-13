05fbceb3f087b6297f6015b409f83e963bfcac63
jaeseuk <sjs4126@naver.com>
2021-10-13 17:09:35 +0900

    #61 [feature] : MainViewModel 작성
    MainViewModel.kt 작성
    검색 및 위치 값 기반 정류장 호출 함수 작성

--- app/src/main/java/com/pns/bbaspassenger/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/pns/bbaspassenger/viewmodel/MainViewModel.kt
@@ -1,41 +1,64 @@
 package com.pns.bbaspassenger.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.pns.bbaspassenger.data.model.BusSystem
+import com.pns.bbaspassenger.data.model.GetStationRequestBody
+import com.pns.bbaspassenger.data.model.SearchRequestBody
+import com.pns.bbaspassenger.repository.BusSystemRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 class MainViewModel : ViewModel() {
-    private val _busSystemData = MutableLiveData<ArrayList<BusSystem>>()
+    private val _busSystemData = MutableLiveData<MutableList<BusSystem>>()
 
-    val busSystemData: LiveData<ArrayList<BusSystem>> = _busSystemData
+    val busSystemData: LiveData<MutableList<BusSystem>> = _busSystemData
 
-    init {
-        _busSystemData.value = arrayListOf(
-            BusSystem("JJB381012020", "bus", "120 (진양호공원-공영차고지)", "일반"),
-            BusSystem(
-                "JJB381247007", "busStop", "경상국립대학교가좌캠퍼스후문 (공영차고지 방면)", "50m", arrayListOf(
-                    BusSystem("JJB381012020", "expand", "120 (진양호공원-공영차고지)", "일반"),
-                    BusSystem("JJB381012110", "expand", "121 (봉전경로회관-진양호차고지)", "일반"),
-                    BusSystem("JJB381012310", "expand", "123 (소곡마을-진양호차고지)", "일반")
-                )
-            )
-        )
-    }
+    fun search(latitude: Double, longitude: Double, query: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                BusSystemRepository.search(SearchRequestBody(latitude, longitude, query)).let { response ->
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            val result = it.result.toMutableList()
 
-    fun search() {
+                            _busSystemData.postValue(result)
+                        }
+                    } else {
+                        Log.d(TAG, "${response.code()}")
+                    }
+                }
+            } catch (e: IOException) {
+                Log.e(TAG, "${e.message} here")
+                _busSystemData.postValue(mutableListOf())
+                e.printStackTrace()
+            }
+        }
     }
 
     fun requestStationByLocation(latitude: Double, longitude: Double) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
+                BusSystemRepository.getStation(GetStationRequestBody(latitude, longitude)).let { response ->
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            val result = it.result.sortedBy { station ->
+                                station.description.dropLast(1).toInt()
+                            }.toMutableList()
 
+                            _busSystemData.postValue(result)
+                        }
+                    } else {
+                        Log.d(TAG, "${response.code()}")
+                    }
+                }
             } catch (e: IOException) {
-
+                Log.e(TAG, "${e.message}")
+                e.printStackTrace()
             }
         }
     }
