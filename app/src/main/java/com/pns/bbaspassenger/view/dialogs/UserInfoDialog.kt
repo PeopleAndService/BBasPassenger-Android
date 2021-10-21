407bacd38a7301e9e9dc54f34990da81660bbc4d
leesh96 <suho2718@gmail.com>
2021-10-22 02:16:09 +0900

    #100 [feature] : 다양한 글씨 크기 지원
    큰 글씨 적용시 일부 ui 잘리는 문제 개선

--- app/src/main/java/com/pns/bbaspassenger/view/dialogs/UserInfoDialog.kt
+++ app/src/main/java/com/pns/bbaspassenger/view/dialogs/UserInfoDialog.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
